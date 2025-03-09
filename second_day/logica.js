28c8f69ea40b3b887b84068f036ee041f55c58a1
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2025-03-09 00:32:35 -0300

    feat: adicionado novas perguntas

--- second_day/logica.js
+++ second_day/logica.js
@@ -11,43 +11,61 @@ let questionAswer = [
   {
     question: "Qual o seu nome?",
     answer: null,
+    type: "text",
+    pattern: "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$"
   },
   {
     question: "Quantos anos você tem?",
     answer: null,
+    type: "text",
+    pattern: "^(150|1[0-4][0-9]|[1-9][0-9]|[0-9])$"
   },
   {
     question: "Qual linguagem de programação você está estudando?",
     answer: null,
+    type: "text",
+    pattern: "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$"
   },
 ];
 
 let index = 0;
 
-showQuestion(index);
-
-buttonSend.addEventListener("click", () => {
-  resgister();
-});
+inputAnswer.setAttribute(
+  "pattern",
+  "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$"
+);
 
 function resgister() {
   let inputAnswer = document.querySelector(".answer");
 
   if (index < questionAswer.length) {
-    questionAswer[index].answer = inputAnswer.value.toupp;
+    questionAswer[index].answer = inputAnswer.value;
     index++;
   }
   
   if (index == questionAswer.length) {
     hideQuestionAswer();
-    showMessageFirst();
-    showButtonNext();
-  }
-
-  if(index < questionAswer.length){
-    showQuestion(index);
+    
+    mesage = "";
+    if(index == 3){
+      mesage = `Olá ${questionAswer[0].answer}, você tem ${questionAswer[1].answer} anos e já está aprendendo ${questionAswer[2].answer}!`
+      showButtonNext();
+    }
+    if(index == 4){
+      mesage = questionAswer[3].answer == 1 ?
+      `Muito bom! Continue estudando e você terá muito sucesso.` :
+      `Ahh que pena... Já tentou aprender outras linguagens?`
+    }
+    showMessageFirst(mesage);
   }
   
+  if(index < questionAswer.length){
+    showQuestion(index);
+    inputAnswer.setAttribute("type", questionAswer[index].type)
+    inputAnswer.setAttribute("pattern", questionAswer[index].pattern)
+  }
+  
+
   cleanInput();
   disableButtonRegister();
   disableButtonRegister();
@@ -57,22 +75,21 @@ function showQuestion(index) {
   console.log(questionAswer)
   labelQuestion.innerHTML = questionAswer[index].question;
 }
+
 function disableButtonRegister() {
   buttonSend.classList.remove("button-send--active");
   buttonSend.setAttribute("disabled", "true");
 }
-
-inputAnswer.setAttribute(
-  "pattern",
-  "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$"
-);
 
 function hideQuestionAswer() {
   labelQuestion.style.display = "none";
   containerAnswer.style.display = "none";
 }
 
-inputAnswer.focus();
+function showQuestionAswer() {
+  labelQuestion.style.display = "flex";
+  containerAnswer.style.display = "flex";
+}
 
 function cleanInput() {
   inputAnswer.value = "";
@@ -85,36 +102,14 @@ function enableButtonRegister() {
   buttonSend.removeAttribute("disabled");
 }
 
-function showMessageFirst() {
-  textMessage.innerHTML = `Olá ${questionAswer[0].answer}, você tem ${questionAswer[1].answer} anos e já está aprendendo ${questionAswer[2].answer}!`;
+function showMessageFirst(mesage) {
+  textMessage.style.display = "block";
+  textMessage.innerHTML = mesage;
 }
 
-iconEraser.addEventListener("click", () => {
-  cleanInput();
-  disableButtonRegister();
-});
-
-inputAnswer.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    resgister();
-  }
-});
-
-inputAnswer.addEventListener("input", (event) => {
-  if (event.target.value.length > 1) {
-    iconEraser.style.display = "block";
-    !event.target.validity.patternMismatch
-      ? enableButtonRegister()
-      : disableButtonRegister();
-  } else {
-    iconEraser.style.display = "none";
-    disableButtonRegister();
-  }
-});
-
-buttonNext.addEventListener("click", () => {
-  console.log("click");
-});
+function hideMessageFirst() {
+  textMessage.style.display = "none";
+}
 
 function showButtonNext() {
   buttonNext.style.display = "flex";
@@ -123,3 +118,57 @@ function showButtonNext() {
 function hideButtonNext() {
   buttonNext.style.display = "none";
 }
+
+function hideButtonNext() {
+  buttonNext.style.display = "none";
+}
+
+
+iconEraser.addEventListener("click", () => {
+  cleanInput();
+  disableButtonRegister();
+});
+
+inputAnswer.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    if (event.target.value.length >= 1 && !event.target.validity.patternMismatch){
+      resgister();
+    }   
+  }
+});
+
+inputAnswer.addEventListener("input", (event) => {
+  if (event.target.value.length >= 1) {
+    iconEraser.style.display = "block";
+    !event.target.validity.patternMismatch
+    ? enableButtonRegister()
+    : disableButtonRegister();
+  } else {
+    iconEraser.style.display = "none";
+    disableButtonRegister();
+  }
+});
+
+buttonNext.addEventListener("click", () => {
+  hideButtonNext()
+  hideMessageFirst()
+  questionAswer.push(
+    { 
+      question: `Você gosta de estudar ${questionAswer[2].answer}? Responda com o número 1 para SIM ou 2 para NÃO.`,
+      answer: null,
+      type: "text",
+      pattern: "^[12]$"
+    })
+    
+    inputAnswer.setAttribute("type", questionAswer[index].type)
+    inputAnswer.setAttribute("pattern", questionAswer[index].pattern)
+  showQuestion(index);
+  showQuestionAswer();
+});
+
+buttonSend.addEventListener("click", () => {
+  resgister();
+});
+
+showQuestion(index);
+inputAnswer.focus();
